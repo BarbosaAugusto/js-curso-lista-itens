6e851db1c03fad276d69de7a90ddfe4f7cf8be8f
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-07-07 20:51:19 -0300

    Leitura dos inputs do formulário

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,58 @@
 class Produto {
 
     constructor() {
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1;
+        this.arrayProdutos = []
+    }
+
+    salvar() {
+        let produto = this.lerDados();
+
+        if(this.validaCampos(produto)) {
+            this.adicionar(produto);
+        }
+
+        console.log( this.arrayProdutos);
+    }
+
+    adicionar(produto) {
+        this.arrayProdutos.push(produto);
+        this.id++;
 
     }
 
-    adicionar() {
-        alert('Vamos adicionar um protudo');
+    lerDados() {
+        let produto = {}
+
+
+        produto.id = this.id;
+        produto.nomeProduto = document.getElementById("produto").value;
+        produto.preco = document.getElementById("preco").value;
+
+        return produto;
     }
 
-    excluir() {
-        alert('Item deletado');
+    validaCampos(produto) {
+        let msg = "";
+
+        if(produto.nomeProduto == "") {
+            msg += "- Informe o nome do produto \n"
+        }
+
+        if(produto.preco == "") {
+            msg += "- Informe o preço do produto \n"
+        }
+
+        if(msg != "") {
+            alert(msg);
+            return false
+        }
+
+        return true;
+    }
+
+    cancelar() {
+
     }
 }
 
