b0f2cb072efdc6f7e0fc7089ba30114981b4014b
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-07-07 17:46:10 -0300

    Initial commit

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+class Produto {
+
+    constructor() {
+        this.id = 0;
+        this.nomeProduto = '';
+        this.valor = 0;
+
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um protudo');
+    }
+
+    excluir() {
+        alert('Item deletado');
+    }
+}
+
+var produto = new Produto();
